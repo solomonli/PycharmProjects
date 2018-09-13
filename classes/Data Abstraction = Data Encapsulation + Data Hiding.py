90211934730d414ea8649f21647f4eef4c14718a
SOLOMON LI <richdad.solomon@gmail.com>
2018-09-13 12:36:36 +1200

    Update Data Abstraction = Data Encapsulation + Data Hiding.py

--- classes/Data Abstraction = Data Encapsulation + Data Hiding.py
+++ classes/Data Abstraction = Data Encapsulation + Data Hiding.py
@@ -1,3 +1,4 @@
+'''
 class Robot:
 
     def __init__(self, name=None):
@@ -23,3 +24,42 @@ y = Robot()
 y.set_name(x.get_name())
 print(y.get_name())
 print(Robot.__dict__)
+'''
+
+
+class Robot:
+ 
+    def __init__(self, 
+                 name=None,
+                 build_year=None):
+        self.name = name   
+        self.build_year = build_year
+        
+    def say_hi(self):
+        if self.name:
+            print("Hi, I am " + self.name)
+        else:
+            print("Hi, I am a robot without a name")
+        if self.build_year:
+            print("I was built in " + str(self.build_year))
+        else:
+            print("I don't know when I was created!")
+            
+    def set_name(self, name):
+        self.name = name
+        
+    def get_name(self):
+        return self.name    
+
+    def set_build_year(self, by):
+        self.build_year = by
+        
+    def get_build_year(self):
+        return self.build_year    
+    
+
+x = Robot("Henry", 2008)
+y = Robot()
+y.set_name("Marvin")
+x.say_hi()
+y.say_hi()
