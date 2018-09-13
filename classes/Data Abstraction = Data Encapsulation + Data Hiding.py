28ff741d19754ac66623408b035621a98401a416
SOLOMON LI <richdad.solomon@gmail.com>
2018-09-13 22:34:09 +1200

    Update Data Abstraction = Data Encapsulation + Data Hiding.py
    another update

--- classes/Data Abstraction = Data Encapsulation + Data Hiding.py
+++ classes/Data Abstraction = Data Encapsulation + Data Hiding.py
@@ -29,37 +29,40 @@ print(Robot.__dict__)
 
 class Robot:
  
-    def __init__(self, 
-                 name=None,
-                 build_year=None):
-        self.name = name   
-        self.build_year = build_year
+    def __init__(self, name=None, build_year=2000):
+        self.__name = name
+        self.__build_year = build_year
         
     def say_hi(self):
-        if self.name:
-            print("Hi, I am " + self.name)
+        if self.__name:
+            print("Hi, I am " + self.__name)
         else:
             print("Hi, I am a robot without a name")
-        if self.build_year:
-            print("I was built in " + str(self.build_year))
-        else:
-            print("I don't know when I was created!")
             
     def set_name(self, name):
-        self.name = name
+        self.__name = name
         
     def get_name(self):
-        return self.name    
+        return self.__name    
 
     def set_build_year(self, by):
-        self.build_year = by
+        self.__build_year = by
         
     def get_build_year(self):
-        return self.build_year    
+        return self.__build_year    
     
+    def __repr__(self):
+        return "Robot('" + self.__name + "', " +  str(self.__build_year) +  ")"
 
-x = Robot("Henry", 2008)
-y = Robot()
-y.set_name("Marvin")
-x.say_hi()
-y.say_hi()
+    def __str__(self):
+        return "Name: " + self.__name + ", Build Year: " +  str(self.__build_year)
+
+     
+if __name__ == "__main__":
+    x = Robot("Marvin", 1979)
+    y = Robot("Caliban", 1943)
+    for rob in [x, y]:
+        rob.say_hi()
+        if rob.get_name() == "Caliban":
+            rob.set_build_year(1993)
+        print("I was built in the year " + str(rob.get_build_year()) + "!")
