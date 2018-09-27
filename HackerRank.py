c5e26f321b07afad302108e3b08b349f3ff4610f
SOLOMON LI <richdad.solomon@gmail.com>
2018-09-27 17:32:33 +1200

    Update HackerRank.py

--- HackerRank.py
+++ HackerRank.py
@@ -45,5 +45,21 @@ Sample Output
 
 if __name__ == '__main__':
     n = int(input())
-    arr = map(int, input().split())     # input e.g. '1 2 3 4 5'      # map return a map object
+#     arr = map(int, input().split())     # input e.g. '1 2 3 4 5' 
+    arr = list(map(int, input().split()))   # risky to omit list()
+    m = max(arr)    # max() doesn't work on map object, sum() does
+  
+    while max(arr) == m:
+        arr.remove(m)
     
+    print(max(arr))
+    
+    
+Given the names and grades for each student in a Physics class of N students, store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+
+Note: If there are multiple students with the same grade, order their names alphabetically and print each name on a new line.
+
+python students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+
+
+
