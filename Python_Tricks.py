b28498c57713b5a0574d39232eefe487352cd263
solomonli <richdad.solomon@gmail.com>
2019-02-11 12:24:00 +1300

    commit at work

--- Python_Tricks.py
+++ Python_Tricks.py
@@ -115,4 +115,28 @@ def emphasis(func):
 def greeting():
     return 'Hello!'
 
+
 print(greeting())
+
+
+def trace(func):
+    def wrapper(*args, **kwargs):
+        print(f'TRACE: calling {func.__name__}() '
+              f'with {args}, {kwargs}')
+
+        original_result = func(*args, **kwargs)
+
+        print(f'TRACE: {func.__name__}() '
+              f'returned {original_result}')
+
+        return original_result
+
+    return wrapper
+
+
+@trace
+def say(name, line):
+    return f'{name}, {line}!'
+
+
+print(say('Jane', 'Come'))
