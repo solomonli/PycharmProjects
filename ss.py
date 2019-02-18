03a9fddad486cc16cd46f18786b681f3cba470c8
solomonli <richdad.solomon@gmail.com>
2019-02-18 18:29:13 +1300

    commit at home

--- ss.py
+++ ss.py
@@ -146,10 +146,26 @@ while True:
     except (ValueError, RuntimeError, TypeError, NameError):    # here only the ValueError may be triggered
         print("Oops!  That was no valid number.  Try again...")
 
+"""
+The try statement works as follows:
 
+First, the try clause (the statement(s) between the try and except keywords) is executed.
+If no exception occurs, the except clause is skipped and execution of the try statement is finished.
+If an exception occurs during execution of the try clause, the rest of the clause is skipped. 
+Then if its type matches the exception named after the except keyword, the except clause is executed, 
+and then execution continues after the try statement.
+If an exception occurs which does not match the exception named in the except clause, 
+it is passed on to outer try statements; if no handler is found, 
+it is an unhandled exception and execution stops with a message as shown above.
+"""
 
+# The raise statement allows the programmer to force a specified exception to occur. For example:
+raise NameError('HiThere')
 
-# everything about the deep / shallow copy
+# how to make shallow copies
+new_list = list(original_list)
+new_dict = dict(original_dict)
+new_set = set(original_set)
 
 
 import copy
