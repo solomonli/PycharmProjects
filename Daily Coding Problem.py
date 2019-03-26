1e5f6004ec5c388afbc09341961c367bd022ce14
solomonli <richdad.solomon@gmail.com>
2019-03-27 08:18:56 +1300

    commit at home

--- Daily Coding Problem.py
+++ Daily Coding Problem.py
@@ -568,11 +568,14 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 
 def display_inventory(inventory):
+
     print("Inventory:")
     item_total = 0
+
     for k, v in inventory.items():
         print(str(v) + ' ' + k)
         item_total += v
+
     return "Total number of items: " + str(item_total)
 
 
---
+++ ss.py
@@ -0,0 +1,6 @@
+table = [['apples', 'oranges', 'cherries', 'banana'],
+        ['Alice', 'Bob', 'Carol', 'David'],
+        ['dogs', 'cats', 'moose', 'goose']]
+
+size = [[None for _ in range(len(table[0]))] for _ in range(len(table))]
+
