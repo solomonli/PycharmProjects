e7a83883f124b0fdf875323ec23c3276d00ba3be
Solomon Li <richdad.solomon@gmail.com>
2019-03-27 13:17:32 +1300

    commit at work

--- Daily Coding Problem.py
+++ Daily Coding Problem.py
@@ -602,3 +602,32 @@ inv = add_to_inventory(inv, dragon_loot)
 
 print(inv)
 
+
+"""
+Table Printer
+Write a function named printTable() that takes a list of lists of strings
+and displays it in a well-organized table with each column right-justified.
+Assume that all the inner lists will contain the same number of strings.
+For example, the value could look like this:
+"""
+table = [['apples', 'oranges', 'cherries', 'banana'],
+        ['Alice', 'Bob', 'Carol', 'David'],
+        ['dogs', 'cats', 'moose', 'goose']]
+
+size = [[None for _ in range(len(table[0]))] for _ in range(len(table))]
+
+for i in range(len(table)):
+    for j in range(len(table[0])):
+        size[i][j] = len(table[i][j])
+
+max_size = [max(row) for row in size]
+
+
+assert len(size) == len(table)
+assert None not in max_size
+
+
+for x in range(len(table[0])):
+    for y in range(len(table)):
+        print(table[y][x].rjust(max_size[y]), end=' ')      # here you can also do " .rjust(max_size[y] + 1), end='' "
+    print('')
--- ss.py
+++
@@ -1,6 +0,0 @@
-table = [['apples', 'oranges', 'cherries', 'banana'],
-        ['Alice', 'Bob', 'Carol', 'David'],
-        ['dogs', 'cats', 'moose', 'goose']]
-
-size = [[None for _ in range(len(table[0]))] for _ in range(len(table))]
-
