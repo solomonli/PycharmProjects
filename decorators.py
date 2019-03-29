15994a505afcafe38841f7bd20ca48fdfeb866fc
Solomon Li <richdad.solomon@gmail.com>
2019-03-29 17:32:30 +1300

    commit at work

---
+++ decorators.py
@@ -0,0 +1,50 @@
+def my_decorator(func):
+	def wrapper():
+		print('Before the func is called')
+		func()
+		print('After the func is called')
+	return wrapper
+
+
+@my_decorator		# it's just a way to say "say_whee = my_decorator(say_whee)"
+def say_whee():
+	print('Whee!')
+
+
+def do_twice(func):
+	def wrapper():
+		func()
+		func()
+	return wrapper
+
+
+@do_twice		# it's just a way to say "say_whee = do_twice(say_whee)"
+def say_whee():
+	print('Whee!')
+
+
+def perf_timer(func):
+
+	def wrapper(*args, **kwargs):
+
+		import datetime
+
+		tik = datetime.datetime.now()
+
+		function_result = func(*args, **kwargs)
+
+		tok = datetime.datetime.now()
+
+		print(f'Elapsed time: {tok - tik}')
+
+		return function_result
+
+	return wrapper
+
+
+# @perf_timer
+def say_whee():
+	print('Whee!')
+
+
+say_whee()
