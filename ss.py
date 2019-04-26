abac86772cd030542be440b4ddb8ccbb021cd346
Solomon Li <richdad.solomon@gmail.com>
2019-04-26 17:03:01 +1200

    commit at work

--- ss.py
+++ ss.py
@@ -67,3 +67,96 @@ The name of a file contains at least a period and an extension.
 The name of a directory or sub-directory will not contain a period.
 """
 
+
+def maximum_subarray(lst, window):
+    """
+    Given an array of integers and a number k, where 1 <= k <= length of the array,
+    compute the maximum values of each subarray of length k.
+
+    For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+
+        10 = max(10, 5, 2)
+        7 = max(5, 2, 7)
+        8 = max(2, 7, 8)
+        8 = max(7, 8, 7)
+
+    Do this in O(n) time and O(k) space. You can modify the input array in-place and you needn't  store the results.
+    You can simply print them out as you compute them.
+    :param lst: a list of int
+    :param window: an integer
+    :return: a list of int
+    """
+    if k >= len(lst):
+        return lst
+
+    res = []
+
+    for i in range(len(lst) - k + 1):
+        res.append(max(lst[i: i+k]))
+
+    return res
+
+
+def house_color(matrix, color_names='ROYGBIV'):
+    """
+    A builder is looking to build a row of N houses that can be of K different colors.
+    He has a goal of minimizing cost while ensuring that no two neighboring houses are of the same color.
+
+    Given an N * K matrix where the Nth row and Kth column represents the cost to build the Nth house with Kth color,
+    return the minimum cost which achieves this goal.
+    :param matrix: a N-length list, with each token of K-length list
+    :param color_names: a string sequence containing the names of the colors
+    :return: an integer
+    """
+
+    num_houses = len(matrix)
+
+    if not num_houses:
+        return []
+
+    num_colors = len(matrix[0])
+
+    totals = [None] * num_houses
+    totals[0] = matrix[0][:]
+
+    # After the first house, dynamic programming does its magic.
+    # At each step, add the price of painting this house
+    # to the minimum of the total painting of the other colors.
+    for i in range(num_houses):
+        totals[i] = [
+            matrix[i][j] +
+            min(totals[i][c] for c in range(num_colors) if c != j) for j in range(num_colors)
+        ]
+
+    # Now we have the totals to paint up to each house.
+    # We'll just work backwards to find the coloring of the houses.
+    colorings = [None] * num_houses
+    colorings[-1] = min(range(num_colors), key=lambda c: totals[-1][c])
+    for house in range(num_houses - 2, -1, -1):
+        # Take the index of the cheapest option (infinite cost
+        # prohibits consecutive houses with the same color)
+        colorings[house] = min(range(num_colors),
+                               key=lambda c: totals[house][c] if c != colorings[house + 1]
+                               else float('Infinity'))
+
+    # Convert color indexes to names
+    return [color_names[colorings[house]] for house in range(num_houses)]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
